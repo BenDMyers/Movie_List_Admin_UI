130d476c141c2830b25238b465efad542eb10a59
Ben Myers <ben.myers@okstate.edu>
2019-06-23 11:34:45 -0500

    EditButton and Search cleanup

--- src/components/MovieList/EditButton.js
+++ src/components/MovieList/EditButton.js
@@ -1,35 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {Button} from 'semantic-ui-react';
 import MaterialButton from '@material-ui/core/Button';
 import 'semantic-ui-css/semantic.min.css';
 
 import './EditButton.styles.css';
 
 const EditButton = (props) => {
-    const handleClick = () => {
-
-    };
-
-    let buttonProps = {variant: 'contained', color: 'primary', onClick: handleClick};
+    let buttonProps = {variant: 'contained', color: 'primary'};
 
     return (
-        <Button as={Link} to={`/edit/${props.movie}`} className="edit-button">
+        <Link to={`/edit/${props.movie}`} className="edit-button">
             <MaterialButton {...buttonProps}>
                 Edit
             </MaterialButton>
-        </Button>
+        </Link>
     );
 };
 
-const mapStateToProps = (state, ownProps) => {
-    const movie = state.movies.list.filter(m => m._id === ownProps.movie)[0];
-    return {
-        numVotes: movie.votes.length,
-        userHasAlreadyVoted: false,
-        uuid: state.user
-    };
-};
-
-export default connect(mapStateToProps)(EditButton);
+export default EditButton;
